4673837ef2aecf19032551ae3b38121367b82e1f
Jenifer Pham <jpa65@sfu.ca>
2019-08-14 17:19:50 -0700

    feat: search component

---
+++ src/components/searchResultComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-row> 
+        <v-col>
+        <v-card
+            class="pa-2"
+            outlined
+            tile
+
+        >
+            <h2 :class="`display-0 font-weight-light pink--text`">{{category}}</h2>
+            {{results.total}} results
+            {{results}}
+        </v-card>
+        <v-spacer></v-spacer>
+        </v-col>
+    </v-row>
+  </div>
+</template>
+
+
+<script>
+import {getTitle} from "../helpers/util"
+import Vue from 'vue'
+export default {
+    name: "SearchResult",
+    props: ['title', 'results'],
+    data () {
+      return {
+        resultLinks: []
+      }
+    },
+    created() {
+        this.formatTitle()
+    },
+    methods: {
+        formatTitle: function() {
+            console.log("in format title")
+            // this.category = this.title.charAt(0).toUpperCase() + this.title.slice(1).toLowerCase()
+            this.category = this.title.toUpperCase() //format this better core => Core dlp => DLP tenx => Tenx
+            return this.category
+        }
+    },
+}
+</script>
