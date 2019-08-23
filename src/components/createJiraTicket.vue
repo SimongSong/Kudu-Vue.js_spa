3875819c34037fbfc03d9fa01de65f655d0b95c6
Jenifer Pham <jpa65@sfu.ca>
2019-08-22 17:25:38 -0700

    started on jira form

---
+++ src/components/createJiraTicket.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>    
+        <v-form >
+            <v-text-field
+                id="title"
+                label="Summary of Ticket"
+                name="title"
+                v-model="defaultTitle"
+            ></v-text-field>
+
+            <v-text-field
+                id="descripion"
+                label="Description"
+                name="description"
+                v-model="defaultDescription"
+                counter
+            ></v-text-field>
+        </v-form>
+        <v-btn>Create Ticket</v-btn>
+    </div>
+</template>
+
+
+
+<script>
+	import Vue from "vue";
+    export default {
+        name: "CreateTicket",
+        props: ["title", "description"],
+        data(){
+            return {
+                defaultTitle : "",
+                defaultDescription: "",
+            }
+        }
+    };
+</script>
